630020a4a1128f5c08505630329d1ae114a1f5c5
GuckTube YT <56192597+GuckTubeYT@users.noreply.github.com>
2022-02-16 19:15:53 +0700

    Update http.cpp

--- proxy/http.cpp
+++ proxy/http.cpp
@@ -58,7 +58,6 @@ uint8_t* read_file(const char* file, uint32_t* size) {
 
 int http::handler(sb_Event* evt)
 {
-	if (evt->type == SB_EV_REQUEST) {
 		PRINT("%s - %s %s\n", evt->address, evt->method, evt->path);
 			PRINT("got server data request.\n");
 			sb_send_status(evt->stream, 200, "OK");
@@ -87,13 +86,6 @@ int http::handler(sb_Event* evt)
 				sb_send_header(evt->stream, "Content-Type", "text/plain");
 				sb_writef(evt->stream, "file not found");
 			}
-		}
-		else {
-			PRINT("unknown request\n");
-			sb_send_status(evt->stream, 200, "OK");
-			sb_send_header(evt->stream, "Content-Type", "text/plain");
-			sb_writef(evt->stream, "unknown");
-		}
 	}
 	return SB_RES_OK;
 }
